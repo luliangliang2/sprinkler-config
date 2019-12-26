a7c6c24748ccd115919be821019cac2a6dfba838
lll <1264864361@qq.com>
2019-12-26 14:55:35 +0800

    测试 ecplise 提交

--- code/sprinkler-all/sprinkler-oath2-client/src/main/java/org/sprinkler/oath2/client/App.java
+++ code/sprinkler-all/sprinkler-oath2-client/src/main/java/org/sprinkler/oath2/client/App.java
@@ -4,10 +4,9 @@ package org.sprinkler.oath2.client;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
 }
