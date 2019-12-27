39c643800f0391a2094b8d981d77d3750e3e3d47
lll <1264864361@qq.com>
2019-12-27 14:41:30 +0800

    commit

--- code/sprinkler-all/sprinkler-oath2-client/src/main/java/org/sprinkler/oath2/client/App.java
+++ code/sprinkler-all/sprinkler-oath2-client/src/main/java/org/sprinkler/oath2/client/App.java
@@ -1,11 +1,18 @@
 package org.sprinkler.oath2.client;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SpringApplication.run(App.class, args);
 	}
 }
