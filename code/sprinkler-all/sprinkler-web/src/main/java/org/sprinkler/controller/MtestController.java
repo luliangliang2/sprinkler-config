81d3a263a63600ec3718a9a07fb9b504ed5896c9
lll <1264864361@qq.com>
2019-12-28 11:06:40 +0800

    1111

--- code/sprinkler-all/sprinkler-web/src/main/java/org/sprinkler/controller/MtestController.java
+++ code/sprinkler-all/sprinkler-web/src/main/java/org/sprinkler/controller/MtestController.java
@@ -1,21 +1,37 @@
 package org.sprinkler.controller;
 
+import java.io.IOException;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="mt")	
+@RequestMapping(value = "mt")
 public class MtestController {
 
 	@Autowired
 	Mtservice mtservice;
-	
-	
 
-	
-	@RequestMapping(value="getPerson")
+
+	@GetMapping("/c/get/{id}")
+	public String get(@PathVariable String id) throws JsonGenerationException, JsonMappingException, IOException {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String user = objectMapper.writeValueAsString(authentication.getPrincipal());
+		
+		System.out.println(user);
+		return "sssss";
+	}
+
+	@RequestMapping(value = "getPerson")
 	public String getPerson() {
 		mtservice.addPerson();
 		return "getPerson";
--- code/sprinkler-all/sprinkler-web/src/main/java/org/sprinkler/web/App.java
+++ code/sprinkler-all/sprinkler-web/src/main/java/org/sprinkler/web/App.java
@@ -1,27 +1,24 @@
 package org.sprinkler.web;
 
-
-
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.sprinkler.orm.annotations.EnableFreeagerAtomikos;
-
-
 
 /**
  * Hello world!
  *
  */
-
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFreeagerAtomikos(dataSource = { "database1", "database2" })
-@ComponentScan(basePackages={"org.sprinkler"})
+@ComponentScan(basePackages = { "org.sprinkler" })
 public class App {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(App.class, args).getEnvironment();
 	}
-	
+
 }
